1e023a5735223c0c71ea37808faad8611d338349
Sufiyan Attar <sufiyanhattar@gmail.com>
2026-06-28 22:47:58 +0530

    vim center and movement

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,4 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.keymap.set("n", "gh", "^")
-vim.keymap.set("n", "gl", "$")
+vim.keymap.set("n", "9", "^")
+vim.keymap.set("n", "0", "$")
+vim.keymap.set("v", "9", "^")
+vim.keymap.set("v", "0", "$")
+
+-- center the cursor
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
