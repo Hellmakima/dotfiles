41d0c28ac2cf97b5e769eceec9be81aff83c3b7a
Sufiyan Attar <sufiyanhattar@gmail.com>
2026-07-05 21:28:16 +0530

    4jul

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>b" .. i, function()
+    require("bufferline").go_to(i, true)
+  end)
+end
+
+vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>")
